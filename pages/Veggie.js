1ee6de8d693dadd3fd752c8e9c2a37c2dfab5913
Phil Metscher <philmetschermailt@gmx.de>
2022-10-04 16:55:20 +0200

    add image to Veggie page

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Veggie() {
-  return <h1>Veggie Page</h1>;
+  return (
+    <>
+      <h1>Veggie Page</h1>
+      <Image
+        src="/images/pizza.jpg"
+        alt="This is Pizza"
+        width={4256}
+        height={2832}
+        layout="responsive"
+      />
+    </>
+  );
 }
